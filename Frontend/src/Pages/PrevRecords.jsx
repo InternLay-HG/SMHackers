c21833d8b56653c4e7055d0746b6a43db7920262
ManasMahato-0 <manasmahato.2004@gmail.com>
2024-11-20 02:37:50 +0530

    button functions added

--- Frontend/src/Pages/PrevRecords.jsx
+++ Frontend/src/Pages/PrevRecords.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState,useRef } from "react"
 import { Header } from "../components/header.jsx"
 import searchicon from '../images/searchicon.png'
 import recordimage from '../images/Records.png'
@@ -11,6 +12,27 @@ import update from "../images/update.svg"
 
 
 function PrevRecords(){
+  const [isDivVisible, setDivVisible] = useState(false);
+  const buttonRef = useRef(null);
+
+  const handleButtonClick = () => {
+    setDivVisible(!isDivVisible);
+  };
+
+  
+    const [chronicVisible, setchronicVisible] = useState(false);
+    const chronicRef = useRef(null);
+  
+    const handleChronic = () => {
+      setchronicVisible(!chronicVisible);
+    };
+
+    const [physicalVisible, setphysicalVisible] = useState(false);
+    const physicalRef = useRef(null);
+  
+    const handlePhysical = () => {
+      setphysicalVisible(!physicalVisible);
+    };
   return(
     <div className="bg-[#f6fffb] h-screen" >
       <Header text={"Login"}></Header>
@@ -39,11 +61,57 @@ function PrevRecords(){
         </div>
       </div>
         <div className="flex justify-evenly gap-0 px-10 py-6 bg-[#F6FFFBC7] h-48 sm:h-32 flex-wrap sm:flex-nowrap">
-          <Button color={"bg-[#E5FDFF]"} text={"Social History"} ></Button>
-          <Button color={"bg-[#E5FDFF]"} text={"Chronic Conditions"} ></Button>
+          <div className="relative">
+          <Button ref={buttonRef} onClick={handleButtonClick} color={"bg-[#E5FDFF]"} text={"Social History"} ></Button>
+          {isDivVisible && (
+              <div className="absolute top-full -left-4 md:-left-2 lg:left-0 mt-2 w-28 md:w-48  lg:w-full  p-2 z-10"
+              style={{
+                top: buttonRef.current ? buttonRef.current.offsetHeight : 0,
+              }}
+              >
+              <div className=" p-3 md:p-5 text-[#92C1B6] sm:text-base  font-inter text-sm bg-white rounded-xl">
+              <p className='font-semibold inline'>Tobacco Use: </p><p className='inline'>1 pack/day </p>
+              <p className='font-semibold inline'>Alcohol Use: </p><p className='inline'>Social Drinker </p>
+              <p className='font-semibold inline'>physical activity: </p><p className='inline'>Minimal </p>
+              <p className='font-semibold inline'>Stress Levels: </p><p className='inline'>High</p>
+              </div>
+              
+              </div>
+              )}
+          </div>
+          <div className="relative">
+          <Button ref={chronicRef} onClick={handleChronic} color={"bg-[#E5FDFF]"} text={"Chronic Conditions"} ></Button>
+          {chronicVisible && (
+              <div className="absolute top-full -left-4 md:-left-2 lg:left-0 mt-2 w-28 md:w-48  lg:w-full  p-2 z-10"
+              style={{
+                top: buttonRef.current ? buttonRef.current.offsetHeight : 0,
+              }}
+              >
+              <div className=" p-3 md:p-5 text-[#92C1B6] sm:text-base  font-inter text-sm bg-white rounded-xl">
+              Asthama
+              </div>
+              
+              </div>
+              )}
+          </div>
           <Button color={"bg-[#E5FDFF]"} text={"Previous Surgery"} ></Button>
           <Button color={"bg-[#E5FDFF]"} text={"Family Medical History"} ></Button>
-          <Button color={"bg-[#E5FDFF]"} text={"Last Physical Exam"}></Button>
+          <div className="relative">
+          <Button ref={physicalRef} onClick={handlePhysical}  color={"bg-[#E5FDFF]"} text={"Last Physical Exam"}></Button>
+          {physicalVisible && (
+              <div className="absolute top-full -left-4 md:-left-2 lg:left-0 mt-2 w-28 md:w-48  lg:w-full  p-2 z-10"
+              style={{
+                top: buttonRef.current ? buttonRef.current.offsetHeight : 0,
+              }}
+              >
+              <div className=" p-3 md:p-5 text-[#92C1B6] sm:text-base  font-inter text-sm bg-white rounded-xl">
+              <p className='font-semibold'>15|10|2024: </p>
+              <p className='inline'>Eye Checkup</p>
+              </div>
+              
+              </div>
+              )}
+          </div>
         </div>
         <div className="flex px-5 md:px-20  justify-evenly gap-6 sm:gap-8 md:gap-10 lg:gap-12 bg-[#f6fffb] overflow-x-scroll no-scrollbar overflow-y-clip ">
           <Card text={"Condition"} ></Card>
